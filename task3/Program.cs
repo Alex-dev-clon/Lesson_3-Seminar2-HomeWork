50af516b1603b5c30b9617d328d3c0c0a8f0e605
Alexei <keilalex@yandex.ru>
2023-10-27 22:42:12 +0700

    finish

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа. 
+Console.WriteLine("Add number");
+int num = Convert.ToInt32(Console.ReadLine());
+int a, b;
+if (num < 100 && num > 9){
+    a = num / 10;
+    b = num % 10;
+    if (a > b) Console.WriteLine(a);
+    else Console.WriteLine(b);
+}
+else Console.WriteLine("Wrong number");
--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+void writer (int n, bool check){
+    if (n > 9){
+        int n_new = n / 10;
+        writer(n_new, false);
+        if (check) Console.WriteLine(n % 10);
+        else Console.Write(n % 10 + ", ");
+    }
+    else Console.Write(n + ", ");
+}
+Console.WriteLine("Add number");
+int num = Convert.ToInt32(Console.ReadLine());
+bool check = true;
+writer(num, check);
